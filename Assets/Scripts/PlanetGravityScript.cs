d96a6e3d52cfadfd4df823b48dedef464782fbb0
Viet Phan <phan_t1@denison.edu>
2017-10-20 20:39:59 -0700

    Cleanup Planet and Spaceship code

--- Assets/Scripts/PlanetGravityScript.cs
+++ Assets/Scripts/PlanetGravityScript.cs
@@ -2,84 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Code 
+
+
 public class PlanetGravityScript : MonoBehaviour {
 
 	public float mass = 100;
-	public float orbiting_velocity;
-	public bool is_being_orbited = false;
-	float updateOrbitStatus;
-	// Use this for initialization
-	void Start () {
-	//	orbiting_velocity = sqrt
-		updateOrbitStatus = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		updateOrbitStatus -= Time.deltaTime;
-	}
-
-
 
 	void OnTriggerStay2D(Collider2D c){
-		Debug.Log ("Gravity is on!");
-
-
-			
 
 		Rigidbody2D spaceShipRigidBody = c.GetComponent <Rigidbody2D> ();
 		float ship_mass = c.GetComponent <SpaceshipMovementScript> ().mass;
 		Vector3 distantVector =  transform.position - c.GetComponent<Transform>().position;
 
-
-			
 		float gravity_force_magnitude = GameOptionsScript.G_CONSTANT * ship_mass * mass / distantVector.sqrMagnitude;
-
 		Vector2 gravity_force = new Vector2 (distantVector.x, distantVector.y).normalized * gravity_force_magnitude ;
-
-		orbiting_velocity = Mathf.Sqrt (GameOptionsScript.G_CONSTANT * mass / distantVector.magnitude); 
-
-		//is_being_orbited = c.GetComponent <SpaceshipMovementScript> ().orbitting;
-		/*spaceShipRigidBody.velocity = closeEnough (spaceShipRigidBody.velocity, new Vector2 (distantVector.x, distantVector.y) );
-		c.GetComponent <SpaceshipMovementScript> ().orbitting = is_being_orbited;
-		*/
-			spaceShipRigidBody.AddForce (gravity_force);
-		
-
-
-
-
-	//	Debug.Log (spaceShipRigidBody.velocity);
+		spaceShipRigidBody.AddForce (gravity_force);
 	}
-	/*
-	Vector2 closeEnough(Vector2 speed, Vector2 p){
-
-
-		return speed;
-		
-		Vector2 orbiting_velocity_vector = new Vector3 (-p.y, p.x);
-
-			
-
-		orbiting_velocity_vector = orbiting_velocity_vector.normalized * orbiting_velocity;
-
-				if (
-			( Mathf.Abs (1 - speed.x/orbiting_velocity_vector.x ) < .2f
-				&& Mathf.Abs (1 - speed.y/orbiting_velocity_vector.y ) < .2f
-			|| 	( Mathf.Abs (1 + speed.x/orbiting_velocity_vector.x) < 0.2f
-					&& Mathf.Abs (1 + speed.y + orbiting_velocity_vector.y) < 0.2f)) ) {
-			Debug.Log ("Orbitting");
-			if (updateOrbitStatus < 0) {
-				is_being_orbited = true;
-				updateOrbitStatus = GameOptionsScript.UPDATE_RATE;
-			}
-			Debug.Log (orbiting_velocity_vector);
-			return orbiting_velocity_vector * orbiting_velocity;
-		}
-		if (is_being_orbited)
-			Debug.Log ("Still orbiting");
-		return orbiting_velocity_vector * orbiting_velocity ;
-		return speed;
-
-	}*/
 }
--- Assets/Scripts/SpaceshipMovementScript.cs
+++ Assets/Scripts/SpaceshipMovementScript.cs
@@ -8,8 +8,6 @@ public class SpaceshipMovementScript : MonoBehaviour {
 
 	bool isActive;
 	public float mass = Mathf.Pow(1, -20);
-	Vector2 speed;
-	public bool orbitting = false;
 
 	public float remainingFuel;
 
@@ -26,60 +24,43 @@ public class SpaceshipMovementScript : MonoBehaviour {
 	void Update () {
 		if (!isActive)
 			return;
+		
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D> ();
 
 		if (Input.GetMouseButton (0)) {
 
-			orbitting = false;
-
-			Vector2 mouse_position_in_game = new Vector2 (
+			Vector2 mousePositionInGame = new Vector2 (
 				(Input.mousePosition.x - GameOptionsScript.SCREEN_CENTER_POINT.x) / GameOptionsScript.UNIT_TO_PIXEL,
 				(Input.mousePosition.y - GameOptionsScript.SCREEN_CENTER_POINT.y) / GameOptionsScript.UNIT_TO_PIXEL);
 
-			speed = new Vector2 (
-				mouse_position_in_game.x, //- transform.position.x,
-			
-				mouse_position_in_game.y //- transform.position.y
+			Vector2 thruster_force = new Vector2 (
+				mousePositionInGame.x, 
+				mousePositionInGame.y 
 			);
+
+
+			rigidBody.AddForce ( thruster_force);
+
+			remainingFuel -= thruster_force.magnitude * Time.deltaTime * 10;
+			GameObject.FindObjectOfType <GamePlayInfoScript> ().BroadcastMessage ("SetFuel", (int)remainingFuel);
+
+
+		} 
 			
-
-			if (speed.x < 0)
-				this.GetComponentInParent<SpriteRenderer> ().flipX = true;
-			else if (speed.x > 0)
-				this.GetComponentInParent<SpriteRenderer> ().flipX = false;
-
-
-
-			//Debug.Log ("Speed: " + speed.ToString());
-			//Debug.Log (Input.mousePosition);
-		} else {
-			speed = Vector2.zero;
-		}
-
-		Rigidbody2D rigidBody = GetComponent<Rigidbody2D> ();
-
-
-		rigidBody.AddForce (speed );
 		transform.rotation = Quaternion.LookRotation (Vector3.forward, rigidBody.velocity);
-
-
-		remainingFuel -= speed.magnitude * Time.deltaTime * 10;
-		GameObject.FindObjectOfType <GamePlayInfoScript> ().BroadcastMessage ("SetFuel", (int)remainingFuel);
-
-
-
 
 		if (remainingFuel <= 0)
 			GameObject.FindObjectOfType <GamePlayManagerScript> ().GameOver ();
-		//GetComponent<Rigidbody2D> ().velocity = new Vector2 (0.0f, -2.0f);
+
 	}
 
 	void OnCollisionEnter2D (Collision2D col) {
+		//death on collision with planet
 		remainingFuel = 0;
 	}
 
 	public void Restart(){
 		isActive = true;
-		speed = Vector2.zero;
 		transform.position = Vector3.zero;
 		remainingFuel = GameOptionsScript.MAX_FUEL_AMOUNT;
 		GetComponent <Rigidbody2D> ().velocity = Vector3.zero;
