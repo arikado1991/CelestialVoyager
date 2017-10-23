60a14928df058e30e63a5015680a529a3cdfcbc5
Viet Phan <phan_t1@denison.edu>
2017-10-22 20:38:08 -0700

    Fixed Timer doesn't reset after Game restart

--- Assets/Scripts/TimerScript.cs
+++ Assets/Scripts/TimerScript.cs
@@ -17,6 +17,7 @@ public class TimerScript : MonoBehaviour {
 		timerDisplayText = transform.Find("TimerDisplayText").GetComponent<Text> ();
 	
 		GamePlayManagerScript.OnSetGameActiveEvent += SetActive;
+		GamePlayManagerScript.OnGameRestartEvent += Restart;
 	
 	}
 
