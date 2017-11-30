efd59ad46361e0800d1fadc0e1b8788fb0b646e2
Viet Phan <phan_t1@denison.edu>
2017-11-30 03:55:16 -0800

    Tweak out of bound fuel drain rate

--- Assets/OutOfBoundScript.cs
+++ Assets/OutOfBoundScript.cs
@@ -5,30 +5,35 @@ using UnityEngine;
 public class OutOfBoundScript : MonoBehaviour {
 
 	const float WARNING_TIME = 3f;
-	const float DRAIN_RATE = 150;
+	const float DRAIN_RATE = 20;
 	SpaceshipInfoScript shipInfo;
 	float warningTime;
-
+	float outOfBoundDuration;
 	public static event EventManagerScript.GetValueDelegate <bool> OutOfBoundEvent;
 	// Use this for initialization
 	void Start () {
 		shipInfo = GamePlayManagerScript.GetInstance ().player.GetComponent <SpaceshipInfoScript> ();
 		warningTime = -10;
+		outOfBoundDuration = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (warningTime > 0)
 			warningTime -= Time.deltaTime;
-		else if (warningTime != -10)
-			shipInfo.fuel -= DRAIN_RATE * Time.deltaTime;
+		else if (warningTime != -10) {
+			shipInfo.fuel -= DRAIN_RATE * Time.deltaTime * outOfBoundDuration * outOfBoundDuration;
+			outOfBoundDuration += Time.deltaTime;
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D col){
 		if (col.gameObject.CompareTag ("Player")) {
 			warningTime = WARNING_TIME;
 			Debug.Log ("Out of bound triggered");
+
 			OutOfBoundEvent (true);
+			outOfBoundDuration = 0;
 		}
 			
 	}
@@ -37,6 +42,7 @@ public class OutOfBoundScript : MonoBehaviour {
 		if (col.gameObject.CompareTag ("Player")) {
 			warningTime = -10;
 			OutOfBoundEvent (false);
+
 		}
 	}
 }
